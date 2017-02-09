5f96276c4e9fe9649baa06cc0909a28fe25e128d
pyh09 <pyh_09@sina.com>
2017-02-09 13:27:27 +0800

    My Website

---
+++ Resume/dsyd/js/main.js
@@ -0,0 +1,17 @@
+
+var tabs_Swiper = new Swiper('.swiper-container', {
+    speed: 500,
+    onSlideChangeStart: function () {
+        $(".tabs .active").removeClass('active');
+        $(".tabs a").eq(tabs_Swiper.activeIndex).addClass('active');
+    }
+});
+$(".tabs a").on('touchstart mousedown', function (e) {
+    e.preventDefault();
+    $(".tabs .active").removeClass('active');
+    $(this).addClass('active');
+    tabs_Swiper.swipeTo($(this).index());
+});
+$(".tabs a").click(function (e) {
+    e.preventDefault();
+});
